84fd9848cc51f454235be620cb241458f9e6f19d
ViniciusPeralta <vinicius-peralta@hotmail.com>
2025-05-09 23:54:19 -0300

    feat: customer page finalized

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
-import './App.css';
+import styles from './App.module.css';
 import Menu from './components/menu/Menu';
 import { useState } from 'react';
 import { PagesEnum, Pages } from './enums/PagesEnum';
+import Customer from './pages/customer/Customer';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function App() {
 
   const [page, setPage] = useState<PagesEnum>(Pages.CYCLES);
 
+  const MenuSpace = (
+    <div className={styles.menuSpace}>
+      <MenuIcon className={styles.openIcon}
+        sx={{ fontSize: 35, mx: 1 }}
+      />
+    </div>
+  );
+
   return (
     <div>
       <Menu changePage={setPage} />
-      {page === Pages.CUSTOMER &&
-          <h1>Customer</h1>
-      }
+      <div className={styles.page}>
+        {MenuSpace}
+        {
+          page === Pages.CUSTOMER &&
+          <Customer />
+        }
+      </div>
     </div>
   )
 }
---
+++ src/pages/customer/Customer.tsx
@@ -0,0 +1,66 @@
+import styles from './Customer.module.css';
+import PageTitle from '../../components/title/PageTitle';
+import BaseTable from '../../components/table/BaseTable';
+import BaseButton from '../../components/button/BaseButton';
+import AddBusinessIcon from '@mui/icons-material/AddBusiness';
+import colors from '../../assets/colors/colors';
+
+const Customer: React.FC = () => {
+
+    const tableHeaders = [
+        'Nome', 'CNPJ'
+    ]
+
+    const tableRows = [
+        { Nome: 'EMPRESA 1', CNPJ: '00.000.000/0001-01' },
+        { Nome: 'EMPRESA 2', CNPJ: '00.000.000/0001-02' },
+        { Nome: 'EMPRESA 3', CNPJ: '00.000.000/0001-03' },
+        { Nome: 'EMPRESA 4', CNPJ: '00.000.000/0001-04' },
+        { Nome: 'EMPRESA 5', CNPJ: '00.000.000/0001-05' },
+        { Nome: 'EMPRESA 6', CNPJ: '00.000.000/0001-06' },
+        { Nome: 'EMPRESA 7', CNPJ: '00.000.000/0001-07' },
+        { Nome: 'EMPRESA 8', CNPJ: '00.000.000/0001-08' },
+        { Nome: 'EMPRESA 9', CNPJ: '00.000.000/0001-09' },
+        { Nome: 'EMPRESA 10', CNPJ: '00.000.000/0001-10' },
+        { Nome: 'EMPRESA 1', CNPJ: '00.000.000/0001-01' },
+        { Nome: 'EMPRESA 2', CNPJ: '00.000.000/0001-02' },
+        { Nome: 'EMPRESA 3', CNPJ: '00.000.000/0001-03' },
+        { Nome: 'EMPRESA 4', CNPJ: '00.000.000/0001-04' },
+        { Nome: 'EMPRESA 5', CNPJ: '00.000.000/0001-05' },
+        { Nome: 'EMPRESA 6', CNPJ: '00.000.000/0001-06' },
+        { Nome: 'EMPRESA 7', CNPJ: '00.000.000/0001-07' },
+        { Nome: 'EMPRESA 8', CNPJ: '00.000.000/0001-08' },
+        { Nome: 'EMPRESA 9', CNPJ: '00.000.000/0001-09' },
+        { Nome: 'EMPRESA 10', CNPJ: '00.000.000/0001-10' }
+    ]
+
+    return (
+        <div className={styles.page}>
+            <PageTitle title='Gerenciamento de Clientes' />
+            <div className={styles.table}>
+                <BaseTable
+                    headers={tableHeaders}
+                    rows={tableRows}
+                    rowKey='CNPJ'
+                    width='70vw'
+                    height='70vh'
+                    overflow='auto'
+                />
+            </div>
+            <div className={styles.button}>
+                <BaseButton
+                    type='button'
+                    text='ADICIONAR CLIENTE'
+                    icon={AddBusinessIcon}
+                    variant='contained'
+                    backGroundColor={colors.gray}
+                    fontSize='0.8vw'
+                    border='1px solid white'
+                    borderRadius='10vw'
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Customer
