2426ab765bf20b98d617656fa1dd72aa6e876e7d
ViniciusPeralta <vinicius-peralta@hotmail.com>
2025-05-10 09:43:05 -0300

    feat: create app toaster

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { PagesEnum, Pages } from './enums/PagesEnum';
 import Customer from './pages/customer/Customer';
 import MenuIcon from '@mui/icons-material/Menu';
+import ToasterProvider from './components/toaster/ToasterProvider';
 
 function App() {
 
@@ -18,16 +19,18 @@ function App() {
   );
 
   return (
-    <div>
-      <Menu changePage={setPage} />
-      <div className={styles.page}>
-        {MenuSpace}
-        {
-          page === Pages.CUSTOMER &&
-          <Customer />
-        }
+    <ToasterProvider>
+      <div>
+        <Menu changePage={setPage} />
+        <div className={styles.page}>
+          {MenuSpace}
+          {
+            page === Pages.CUSTOMER &&
+            <Customer />
+          }
+        </div>
       </div>
-    </div>
+    </ToasterProvider>
   )
 }
 
