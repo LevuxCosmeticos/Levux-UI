6907bd10a672edc0a6fd60472986f5735e7b5aa2
ViniciusPeralta <vinicius-peralta@hotmail.com>
2025-05-20 12:12:10 -0300

    feat: create cycle table structure

---
+++ src/components/table/cycle/CycleTableHeader.tsx
@@ -0,0 +1,28 @@
+import TableHeader from "../../../dto/table/TableHeader"
+
+export const cycleTableHeader: TableHeader[] = [
+    {
+        key: 'produto',
+        label: 'Produto'
+    },
+    {
+        key: 'saldo_inicial',
+        label: 'Saldo Inicial'
+    },
+    {
+        key: 'levantamento',
+        label: 'Levantamento'
+    },
+    {
+        key: 'vendido',
+        label: 'Vendido'
+    },
+    {
+        key: 'reposicao',
+        label: 'Reposição'
+    },
+    {
+        key: 'saldo_final',
+        label: 'Saldo Final'
+    }
+]
