f0ba3479eb23e36d61903a7224e5b27935a09973
ViniciusPeralta <vinicius-peralta@hotmail.com>
2025-05-19 23:19:29 -0300

    feat: create cycle response dto

---
+++ src/dto/cycle/filter/CycleBalanceResponse.tsx
@@ -0,0 +1,9 @@
+export interface CycleBalanceResponse {
+    productId: number;
+    productName: string;
+    initialBalance: number;
+    lift: number;
+    sold: number;
+    replacement: number;
+    finalBalance: number;
+}
